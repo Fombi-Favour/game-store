0ffb529383eda333e71074c80b80f82c5a00eebb
Fombi-Favour <fombifavour@gmail.com>
2023-10-10 13:59:23 +0100

    Create gameTitle.js

---
+++ src/components/gameTitle.js
@@ -0,0 +1,64 @@
+const imageContinents = {
+  batman: {
+    title: 'Batman',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQccGCSlaMmm8FPXZvNtXotWd0XNu5FdS3nt7i5RrP-&s',
+  },
+  fifa: {
+    title: 'FIFA',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROALlqCXNkkPHNsby50CIewZRnlHtwkcfXB2KWZjsv&s',
+  },
+  wwe: {
+    title: 'WWE',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToXVRVqBzh73JjRXXxQ9JHuDevNGDgSKbgnouGFBB4gQ&s',
+  },
+  action: {
+    title: 'Action',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0eadiEIuzUFgjMaBCTrqeSpOh8tPwbMcmK42Bo74&s',
+  },
+  nba: {
+    title: 'NBA',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4DBmqoEb1O64JIaBuNu8REX8_2i2CeA2T4WmhPDOm&s',
+  },
+  f1: {
+    title: 'F1 (Formula 1)',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3hwvwPiU9hu4ZrtsrYiiMrn7dDh_N94Ch4gddvoka&s',
+  },
+  nfs: {
+    title: 'Need For Speed (NFS)',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcgXJIBAiRQopRvOvXUejY_u0cRXJPwKlvUH_7OmpL&s',
+  },
+  lego: {
+    title: 'LEGO',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLvZqEw16v5FazxaqVSomjxCIfLxraWG1_yuv_QvKx&s',
+  },
+  marvels: {
+    title: 'Marvel',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMU8VKQZLC2L_aQGN67daJFhBy1HmcJjeGJmJmZZAJXe94I1JbOHFkEgtWLE9bUsjfrIo&usqp=CAU',
+  },
+  'call-of-duty': {
+    title: 'Call of Duty',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX9GBEbVsM30VOqoMx7MX0h62AFdSXKYNjdvknnRhK&s',
+  },
+  'mortal-kombat': {
+    title: 'Mortal Kombat',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEAjowpvpJR3eZ2apiSv3SevaNPRJxr4daPtfhwR9s&s',
+  },
+  'devil-may-cry': {
+    title: 'Devil May Cry',
+    src: 'https://static.wikia.nocookie.net/logopedia/images/6/67/DMC5_logo.png/revision/latest?cb=20210216173855',
+  },
+  hitman: {
+    title: 'Hitman',
+    src: 'https://e7.pngegg.com/pngimages/671/182/png-clipart-hitman-logo-brand-design-font-hitman-hart-wrestling-with-shadows-angle-white-thumbnail.png',
+  },
+  'street-fighter': {
+    title: 'Street Fighter',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTl7oWvd69lTVtjnIHdED1o_9CGa8MVDbQmA3aP986&s',
+  },
+  tekken: {
+    title: 'Tekken',
+    src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVl058rqUodcHPz2_xiBM72vQTOWS5ZEtyEYjQWvEulQ&s',
+  },
+};
+
+export default imageContinents;
