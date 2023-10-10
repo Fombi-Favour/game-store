fc3b4cba7241884db4450969638353925d9f991b
Fombi-Favour <fombifavour@gmail.com>
2023-10-10 14:01:07 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import homeReducer from './home/homeSlice';
 import detailsReducer from './details/detailsSlice';
 
 const store = configureStore({
   reducer: {
-    home: homeReducer,
     details: detailsReducer,
   },
 });
