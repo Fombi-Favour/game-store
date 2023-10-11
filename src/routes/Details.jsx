fc14d578e581f204131b3ecbd65183d7d7359b90
Fombi-Favour <fombifavour@gmail.com>
2023-10-11 21:29:20 +0100

    Update Details.jsx

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGames } from '../redux/details/detailsSlice';
 
-const Details = () => (
-  <div>Details</div>
-);
+const Details = () => {
+  const dispatch = useDispatch();
+  const { name } = useParams();
+  const { games, isLoading } = useSelector((state) => state.details);
+
+  useEffect(() => {
+    dispatch(fetchGames(name));
+  }, [dispatch, name]);
+
+  return (
+    <section className="bg-[#fb5092] text-white">
+      {/* title sub-section */}
+      <div className="flex items-center justify-center gap-8 p-2">
+        <span>image</span>
+        <div className="flex flex-col">
+          <h3 className="text-[1.654rem] font-semibold">{name}</h3>
+          <span className="text-[1.174rem]">{`${games.length} Games`}</span>
+        </div>
+      </div>
+      {/* status sub-section */}
+      <div className="flex bg-[#da2d72] p-1">
+        <h3 className="uppercase">
+          {`status of ${name} games`}
+        </h3>
+      </div>
+      {/* games list sub-section */}
+      {isLoading ? (
+        <div className="flex items-center justify-center mt-20 gap-3">
+          <div className="border-8 border-white border-t-[#da2d72] w-20 h-20 text-transparent rounded-full animate-spin">100%</div>
+          <h2 className="text-4xl text-white">Loading...</h2>
+        </div>
+      ) : (
+        <span>GameList</span>
+      )}
+    </section>
+  );
+};
 
 export default Details;
