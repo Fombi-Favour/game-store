e81745c3ca1033e4c899087f7f72d699c0baa458
Fombi-Favour <fombifavour@gmail.com>
2023-10-12 22:43:39 +0100

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,13 +3,16 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { FaArrowCircleRight } from 'react-icons/fa';
 
-const Categories = ({ title, name, src }) => (
+const Categories = ({
+  id, title, name, src,
+}) => (
   <NavLink
     to={`details/${name}`}
     className="flex flex-col items-end gap-2 p-2 h-[18rem] rounded-xl bg-[#da2d72] shadow-lg transition-all hover:transition-all hover:shadow-2xl hover:shadow-pink-400"
   >
     <div>
       <FaArrowCircleRight />
+      {id}
     </div>
     <div className="flex flex-col items-center gap-2 justify-between">
       <img src={src} alt={title} className=" w-60 h-40" />
@@ -19,6 +22,7 @@ const Categories = ({ title, name, src }) => (
 );
 
 Categories.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
